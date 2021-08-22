ff65cc7671cdcd65928778fa492f429dae550c31
Adrian <adrian.kloc47@gmail.com>
2021-08-22 11:23:44 +0200

    #12 Main page
    - created main page component
    - created initial layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,22 @@ import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from '../shared/shared/shared.module';
+import { MainComponent } from './main/main.component';
 
 registerLocaleData(en);
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [
+		AppComponent,
+		MainComponent
+	],
 	imports: [
 		BrowserModule,
 		FormsModule,
 		HttpClientModule,
-		BrowserAnimationsModule
+		BrowserAnimationsModule,
+		SharedModule
 	],
 	providers: [{ provide: NZ_I18N, useValue: en_GB }],
 	bootstrap: [AppComponent]
